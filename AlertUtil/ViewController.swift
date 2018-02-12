151a28a3d8f2720a0d4495b25f463ecee2c361bc
liyang <liyang@hd123.com>
2018-02-12 13:27:48 +0800

    修改控制器颜色为灰色

--- AlertUtil/ViewController.swift
+++ AlertUtil/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         self.view.backgroundColor = UIColor.red
-        
+        view.backgroundColor = UIColor.gray
     }
 
     override func didReceiveMemoryWarning() {
