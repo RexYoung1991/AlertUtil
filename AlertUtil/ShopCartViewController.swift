6a9bc5df97dbdc9fe250d4aa579b943ce4c91a4c
liyang <liyang@hd123.com>
2018-02-12 13:19:52 +0800

    Create shopcartVC

---
+++ AlertUtil/ShopCartViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ShopCartViewController.swift
+//  AlertUtil
+//
+//  Created by Young on 2018/2/12.
+//  Copyright © 2018年 liyang. All rights reserved.
+//
+
+import UIKit
+
+class ShopCartViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        view.backgroundColor = UIColor.green
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
